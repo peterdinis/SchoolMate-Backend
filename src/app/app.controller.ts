42c0e3267bba8f414082ad27dd22beb152b704ca
Peter Dinis <peterdinis0@pobox.sk>
2024-09-25 15:48:04 +0200

    infra: Setup swagger

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Get a hello message' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully returned hello message.',
+  })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+
+    .setTitle('School Mate API')
+    .setDescription('Documentation for school mate API')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   await app.listen(3000);
 }
